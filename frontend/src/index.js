18a68f5d138c31ab44c2a04d94a3fe8703c9f349
EwanMcA <ewan.m@hotmail.com>
2021-02-20 21:34:40 +0000

    Add lint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/app';
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/app";
 
-const rootEl = document.getElementById('app');
+const rootEl = document.getElementById("app");
 
 
 render(<App />, rootEl);
